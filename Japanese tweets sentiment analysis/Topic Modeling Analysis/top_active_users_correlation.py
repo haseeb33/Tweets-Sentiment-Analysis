5183cd1e30f15b0b8e9c8b3d7b41d1edd896a2b8
Muhammad Haseeb UR Rehman Khan <haseebulr@yahoo.com>
2019-11-20 12:53:40 +0900

    Correlation code for top 65 users

---
+++ Japanese tweets sentiment analysis/Topic Modeling Analysis/top_active_users_correlation.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 17 15:26:38 2019
+
+@author: khan
+"""
+
+import pandas as pd
+
+theta_df = pd.read_excel("NLP_JP100Topics65users.xlsx", sheet_name = "ThetaValues", index_col=0)
+extract_top_65 = pd.read_excel("result_sentiment_analysis.xlsx")
+original_df = pd.read_excel("Final1.xlsx")
+a = extract_top_65.loc[extract_top_65[">=50"]>=0, "通し番号"]
+imp_df = original_df.loc[original_df["通し番号"].isin(a.to_frame()["通し番号"])]
+
+H_col = imp_df["一般的信頼合計"].values.tolist()
+I_col = imp_df["社会的スキル合計"].values.tolist()
+J_col = imp_df["心理的幸福感合計"].values.tolist()
+
+theta_df["H"] = H_col
+theta_df["I"] = I_col
+theta_df["J"] = J_col
+corr = theta_df.corr()
+
+new_df = pd.DataFrame([corr["H"], corr["I"], corr["J"]])
+new_df = new_df.T
+new_df = new_df.sort_values(by=['H','I','J'], ascending=False)
+new_df.to_excel("CorrWithTopics.xlsx")
