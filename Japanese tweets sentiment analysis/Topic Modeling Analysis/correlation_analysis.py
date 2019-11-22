6300dba4af1da5c0768cad93f74e5a26fb754715
Muhammad Haseeb UR Rehman Khan <haseebulr@yahoo.com>
2019-11-22 21:19:00 +0900

    Minor fix

--- Japanese tweets sentiment analysis/Topic Modeling Analysis/correlation_analysis.py
+++ Japanese tweets sentiment analysis/Topic Modeling Analysis/correlation_analysis.py
@@ -9,7 +9,7 @@ import pandas as pd
 
 theta_df = pd.read_excel("NLP_JP100topic.xlsx", sheet_name = "ThetaValues", index_col=0)
 original_df = pd.read_excel("Final1.xlsx", usecols = ["通し番号","一般的信頼合計", "社会的スキル合計", "心理的幸福感合計"])
-imp_df = original_df.loc[original_df["通し番号"].isin(theta_df[300])]
+imp_df = original_df.loc[original_df["通し番号"].isin(theta_df[100])]
 H_col = imp_df["一般的信頼合計"].values.tolist()
 I_col = imp_df["社会的スキル合計"].values.tolist()
 J_col = imp_df["心理的幸福感合計"].values.tolist()
